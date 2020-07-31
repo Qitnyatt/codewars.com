258c35c7fd20878748aa0aa0004b4d0f699c86e0
Qitnyatt <Qitnyatt@protonmail.com>
2020-07-31 18:26:56 +0000

    Improved javascript::8 kyu::short_long_short

--- javascript/kyu_8/short_long_short.js
+++ javascript/kyu_8/short_long_short.js
@@ -1,1 +1,3 @@
+// https://www.codewars.com/kata/50654ddff44f800200000007/train/javascript
+
 const solution = (a, b) => `${a.length > b.length ? b : a}${a.length < b.length ? b : a}${a.length > b.length ? b : a}`;
