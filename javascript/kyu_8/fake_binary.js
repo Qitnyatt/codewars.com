de93f38047902d3401acea914770e0914b3d1621
Qitnyatt <Qitnyatt@protonmail.com>
2020-07-31 18:26:09 +0000

    Improved javascript::8 kyu::fake_binary

--- javascript/kyu_8/fake_binary.js
+++ javascript/kyu_8/fake_binary.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+
 const fakeBin = (x) => x
     .split('')
     .map(currentValue => parseInt(currentValue, 10) < 5 ? '0' : '1')
