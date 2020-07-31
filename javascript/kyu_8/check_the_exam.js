f14b46c1a4248c82c22a052c2e266eb1dcef342f
Qitnyatt <Qitnyatt@protonmail.com>
2020-07-31 18:28:34 +0000

    Improved javascript::8 kyu::check_the_exam

--- javascript/kyu_8/check_the_exam.js
+++ javascript/kyu_8/check_the_exam.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/javascript
+
 function checkExam(array1, array2) {
     let result = 0;
     for (let i = 0; i < array1.length; i++) {
