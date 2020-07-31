68a0fc76343cd1387ca916b89303df80c409b8ab
Qitnyatt <Qitnyatt@protonmail.com>
2020-07-31 18:31:28 +0000

    Improved javascript::8 kyu::find_multiples_of_a_number

--- javascript/kyu_8/find_multiples_of_a_number.js
+++ javascript/kyu_8/find_multiples_of_a_number.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/58ca658cc0d6401f2700045f/train/javascript
+
 const findMultiples = (integer, limit) => {
     const result = [];
     for (let i = integer; i <= limit; i += integer) {
